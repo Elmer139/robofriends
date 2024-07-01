4cd12692b1f2108a8f5b6be8da1316d8e1d87199
Shawn Conrad <mrshawnconrad@gmail.com>
2024-07-01 11:52:31 -0400

    adjusted margins around cards

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma4 tc grow bw2 shadow-5">
+    <div className="bg-light-green dib br3 pa3 ma3 tc grow bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?100x100`} />
       <div>
         <h2>{name}</h2>
